d7435ddb67098a1bf7e416d6e8e531f3f524c072
rajesh <rajeshkaramsetty1999@gmail.com>
2026-01-02 21:55:17 +0530

    added content

---
+++ 60-acm/provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.19.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "84s-demo-remotest-comp"
+    #bucket       = "84s-demo-remotest"
+    key          = "roboshop-dev-acm"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
---
+++ 60-acm/variables.tf
@@ -0,0 +1,17 @@
+variable "project" {
+  default = "roboshop"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+
+
+variable "zone_id" {
+  default = "Z01448731VZNPW3ACOZ36"
+}
+
+variable "zone_name" {
+  default = "rajeshit.space"
+}
