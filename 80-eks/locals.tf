6d7cf25e0b022f5791bcdec7ebab628550c18e3c
rajesh <rajeshkaramsetty1999@gmail.com>
2026-01-03 20:48:35 +0530

    added content

--- 80-eks/locals.tf
+++ 80-eks/locals.tf
@@ -1,6 +1,6 @@
 locals {
   vpc_id                  = data.aws_ssm_parameter.vpc_id.value
-  private_subnet_ids      = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
+  private_subnet_ids      = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
   eks_control_plane_sg_id = data.aws_ssm_parameter.eks_control_plane_sg_id.value
   eks_nodes_sg_id         = data.aws_ssm_parameter.eks_nodes_sg_id.value
 
