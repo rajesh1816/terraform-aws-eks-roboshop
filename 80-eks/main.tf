facc968cc1f425600977064ac87025abc356e57e
rajesh <rajeshkaramsetty1999@gmail.com>
2026-01-18 14:32:04 +0530

    added content

--- 80-eks/main.tf
+++ 80-eks/main.tf
@@ -39,7 +39,7 @@ module "eks" {
     blue = {
       # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
       ami_type       = "AL2023_x86_64_STANDARD"
-      instance_types = ["t3.small"]
+      instance_types = ["m5.xlarge"]
 
       min_size     = 1
       max_size     = 5
