a5e8f9db744a8f6b01a6d73aaee8e7f30572050d
rajesh <rajeshkaramsetty1999@gmail.com>
2026-01-18 14:15:18 +0530

    added content

--- 20-bastion/data.tf
+++ 20-bastion/data.tf
@@ -3,12 +3,22 @@ data "aws_ssm_parameter" "public_subnet_ids" {
 }
 
 data "aws_ami" "joindevops" {
-  most_recent = true
   owners      = ["973714476881"]
+  most_recent = true
 
   filter {
     name   = "name"
     values = ["RHEL-9-DevOps-Practice"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
 }
 
